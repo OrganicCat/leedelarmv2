562b03dd2db67e95b974de237fae7e27b581e732
Lee Delarm <lee.delarm@gmail.com>
2022-04-12 20:58:31 -0400

    Add animated line between slide menu items

--- src/SlideMenu/SlideMenu.tsx
+++ src/SlideMenu/SlideMenu.tsx
@@ -4,13 +4,13 @@ function SlideMenu() {
     return (
         <div className="slide-menu">
             <div className="interactive-icon">About Me</div>
-            <div className="vertical-line"></div>
+            <div className="vertical-line"><div className="moving-line"></div></div>
             <div className="interactive-icon">Projects</div>
-            <div className="vertical-line"></div>
+            <div className="vertical-line"><div className="moving-line"></div></div>
             <div className="interactive-icon">Interests</div>
-            <div className="vertical-line"></div>
+            <div className="vertical-line"><div className="moving-line"></div></div>
             <div className="interactive-icon">Blog (TBA)</div>
-            <div className="vertical-line"></div>
+            <div className="vertical-line"><div className="moving-line"></div></div>
             <div className="interactive-icon">Resume</div>
         </div>
     )
